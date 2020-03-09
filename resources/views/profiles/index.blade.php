f8b172c533de4c4531a9a7a7b002f50314c1b10a
sophie <s.hautcoeur@gmail.com>
2020-03-09 15:03:16 +0200

    showing posts on profile page

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,10 +9,10 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="#">Add new post</a>
+                <a href="/p/create">Add new post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>155</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>235</strong> followers</div>
                 <div class="pr-5"><strong>215</strong> following</div>
             </div>
@@ -22,15 +22,12 @@
         </div>
     </div>
     <div class="row pt-5">
-        <div class="col-4">
-            <img src="https://thumbs.dreamstime.com/b/spring-flowers-blue-crocuses-drops-water-backgro-background-tracks-rain-113784722.jpg" class="w-100">
+        @foreach($user->posts as $post)
+
+        <div class="col-4 pb-4">
+            <img src="/storage/{{ $post->image }}" class="w-100">
         </div>
-        <div class="col-4">    
-            <img src="https://thumbs.dreamstime.com/b/spring-flowers-blue-crocuses-drops-water-backgro-background-tracks-rain-113784722.jpg" class="w-100">
-        </div>
-        <div class="col-4">    
-            <img src="https://thumbs.dreamstime.com/b/spring-flowers-blue-crocuses-drops-water-backgro-background-tracks-rain-113784722.jpg" class="w-100">
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
