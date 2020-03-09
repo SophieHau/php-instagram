9be2d1e6ceabb1f5099fa03f2a7b6691d9744652
sophie <s.hautcoeur@gmail.com>
2020-03-09 18:12:17 +0200

    adding default profile image

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,11 @@ class Profile extends Model
 {
     protected $guarded = [];
     
+    public function profileImage() {
+        $imagePath = ($this->image) ? $this->image : '/profile/oWy7KHPqMsmw3OOhCdpSqBZpIQYYBOGV6XiTayrL.jpeg';
+        return '/storage/' . $imagePath;
+    }
+
     public function user() {
         return $this->belongsto(User::class);
     }
